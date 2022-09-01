74216d9c8db9b089cc0b8ae91a659288b68459e3
Ali Koudar Mahamat <alikhoudar@gmail.com>
2022-09-01 15:39:39 +0100

    Initial commit

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer>
+          &copy; 2022 Ali development. All rights reserved.
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/MainContent.js
@@ -0,0 +1,17 @@
+function MainContent() {
+    return (
+      <div>
+        <h1>Reason I'm excited to learn React</h1>
+        <ol>
+            <li>It's a popular libray, so I'll be able to fit in with the cool kids</li>
+            <li>Created and maintained by a big company</li>
+            <li>It's declarative</li>
+            <li>It's composable</li>
+            <li>I'm more likely to get a jobs as a developer if I know React</li>
+            
+        </ol>
+      </div>
+    )
+  };
+
+  export default MainContent;
